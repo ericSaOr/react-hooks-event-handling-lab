e421e7afe78f3c0c29ed4c757ecaf35c1f7253aa
Eric Sanchez <ericfsod@gmail.com>
2021-07-25 19:44:09 -0400

    Completed Assignment

--- src/components/EyesOnMe.js
+++ src/components/EyesOnMe.js
@@ -1,14 +1,9 @@
 function EyesOnMe() {
-	function handleButton(event) {
-        const isOnFocus= event.target.onFocus;
-       return {isOnFocus ? console.log('Good!'):console.log('Hey! Eyes on me')}
-		
 	return (
-		<div>
-			<button onClick={handleButton}>'Eyes on me'</button>
-		</div>
+        <button onFocus={() => console.log('Good!')} onBlur={() => console.log('Hey! Eyes on me!')}>
+			'Eyes on me'
+		</button>
 	);
-  }
 }
 
 export default EyesOnMe;
