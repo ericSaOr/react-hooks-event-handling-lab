5c55da121e73016eb59c764f236d3fad4fe1f905
Eric Sanchez <ericfsod@gmail.com>
2021-07-25 18:29:56 -0400

    pushing errors

--- src/components/EyesOnMe.js
+++ src/components/EyesOnMe.js
@@ -1,1 +1,13 @@
-// Code EyesOnMe Component Here
+wction EyesOnMe() {
+	function handleButton(event) {
+        const isOnFocus= event.target.onFocus;
+        {isOnFocus ? console.log('Good!'):console.log('Hey! Eyes on me')}
+		
+	return (
+		<div>
+			<button onClick={()=>{handleButton}}>'Eyes on me'</button>
+		</div>
+	);
+}
+
+export default EyesOnMe;
--- src/components/Keypad.js
+++ src/components/Keypad.js
@@ -1,1 +1,15 @@
 // Code Keypad Component Here
+import react from 'react';
+
+function Keypad() {
+	function onKeypadChange() {
+		console.log('Entering password...');
+	}
+	return (
+		<div>
+			<input type="password" onChange={onKeypadChange} />
+		</div>
+	);
+}
+
+export default Keypad;
