069040962ce9ca4e735c5756616499c42d7558ae
Eric Sanchez <ericfsod@gmail.com>
2021-07-25 18:39:54 -0400

    errors again

--- src/components/EyesOnMe.js
+++ src/components/EyesOnMe.js
@@ -1,13 +1,14 @@
-wction EyesOnMe() {
+function EyesOnMe() {
 	function handleButton(event) {
         const isOnFocus= event.target.onFocus;
-        {isOnFocus ? console.log('Good!'):console.log('Hey! Eyes on me')}
+       return {isOnFocus ? console.log('Good!'):console.log('Hey! Eyes on me')}
 		
 	return (
 		<div>
-			<button onClick={()=>{handleButton}}>'Eyes on me'</button>
+			<button onClick={handleButton}>'Eyes on me'</button>
 		</div>
 	);
+  }
 }
 
 export default EyesOnMe;
